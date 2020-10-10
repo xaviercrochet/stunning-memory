c2f6aff8fe6fac35a33b48b346040226dd494c5b
Xavier <1391918+xaviercrochet@users.noreply.github.com>
2020-10-10 13:09:31 +0200

    improved ui layout

--- app/services/place_service.rb
+++ app/services/place_service.rb
@@ -4,6 +4,40 @@ require 'open-uri'
 module PlaceService
     class Place
         attr_accessor :name, :location, :opening_hours
+
+        def initialize
+            @opening_hours = []
+        end
+    end
+
+    class OpeningHour
+        attr_accessor :days, :hours
+
+        def initialize
+            @days = []
+            @hours = []
+        end
+
+        def getDaysHeading
+            if @days.length == 0 
+                return ''
+            elsif @days.length == 1
+                return @days[0]
+            else
+                return @days[0] + " - " + @days[@days.length - 1]
+            end
+        end
+
+        def isToday?
+            today = Date.today.strftime("%A")
+            if not @days.index(today.downcase).nil?
+                return true
+            else
+                return false
+            end
+        end
+
+
     end
 
     def self.query(id)
@@ -27,11 +61,23 @@ module PlaceService
         end
         if not response.blank?
             result = JSON.parse(response.to_str)
-            puts "service: " +  result.to_s
             place = PlaceService::Place.new
             place.name = result["Name"]
             place.location = result["Location"]
-            place.opening_hours = result["OpeningHours"]
+            result["OpeningHours"].each do | openingHourJSON |
+                openingHour = PlaceService::OpeningHour.new
+                openingHourJSON["Days"].each do | dayJSON|
+                    openingHour.days << dayJSON
+                end
+
+                openingHourJSON["Hours"].each do | hourJSON|
+                    openingHour.hours << hourJSON
+                end
+                place.opening_hours << openingHour
+
+            end
+            puts place
+            
             return place
         end
     end
